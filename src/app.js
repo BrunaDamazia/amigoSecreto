7eafcc995f0d6e08f15b29ea8d734d779ebdd3ba
Bruna Damazia <brunadamazia.dev@gmail.com>
2025-03-18 17:43:36 -0300

    primeira versão javascript

--- src/app.js
+++ src/app.js
@@ -1,1 +1,44 @@
+const inputWrapper = document.querySelector('.input-wrapper');
+const friendNameInput = document.getElementById('friendName');
+const buttonAdd = document.getElementById('button-add');
+const buttonDraw = document.getElementById('button-draw');
+const listFriends = document.getElementById('listFriends');
+const result = document.getElementById('result');
 
+let list = [];
+
+function addFriend() {
+const name = friendNameInput.value.trim();
+  if (name) {
+    list.push(name)
+    friendNameInput.value = '';
+    updateListFriends();
+  } else {
+    
+  }
+}
+
+function updateListFriends() {
+  listFriends.textContent = '';
+  list.forEach((friend, index) => {
+    const li = document.createElement('li');
+    li.textContent = `${index + 1}. ${friend}`;
+    listFriends.appendChild(li);
+  })
+}  
+
+function drawFriend() {
+    if(list.length > 1) {
+      const random = Math.floor(Math.random() * list.length);
+      const winner = list[random];
+      result.textContent = `O amigo secreto sorteado é ${winner}`;
+    } else if(list.length < 2){
+      result.textContent = 'Adicione mais amigos para sortear.'
+    }
+    else {
+      result.textContent = 'Adicione amigos para sortear.'
+    }
+  }
+  
+inputWrapper.addEventListener('Submit', addFriend);
+buttonDraw.addEventListener('Click', drawFriend);
